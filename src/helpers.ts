f7000e99798208f02ea46088aa7bf3c79f32d089
Viktor Loktionov <viktor.loktionov@bringg.com>
2023-03-10 18:23:16 +0100

    ci: trigger ci

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,1 +1,6 @@
+import { BaseCubeMember } from "@cubejs-client/core";
+
 export const isDefined = <T>(value: T | null | undefined): value is T => value !== null && value !== undefined;
+
+export const isBaseCubeMember = (member: unknown): member is BaseCubeMember =>
+  typeof member === "object" && member !== null && "type" in member && "name" in member;
